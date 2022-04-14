441dae49f19ef761bb9766c9a0a2ee61deebcde8
username <chen_jiawei666@163.com>
2022-04-14 15:45:21 +0800

    新增企业微信小程序以及群机器人报告通知功能

--- testcase/conftest.py
+++ testcase/conftest.py
@@ -5,12 +5,14 @@ import os
 import pytest
 from common.Log import log
 from common.Send_Email import Send_email
-from common.WorkWeChat import send_workwhat
+from common.WorkWeChat import send_workwhat, send_workwhat_robot
 from common.serverchanConf import sendServerChan
 from common.yaml_util1 import clear_yaml, read_yamlcase
 import time
 
 BASE_PATH = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+
+
 def get_data(yaml_file_name):
     try:
         data_file_path = os.path.join(BASE_PATH, "data_file", yaml_file_name)
@@ -20,21 +22,23 @@ def get_data(yaml_file_name):
     else:
         return yaml_data
 
+
 api_data = get_data("test_case.yaml")
 
-@pytest.fixture(scope="session",autouse=True)
+
+@pytest.fixture(scope="session", autouse=True)
 def execute_database_sql():
     log.info("~~~~~~~~~~在所有请求之前执行一次~~~~~~~~~~~")
     clear_yaml('/extract_token.yaml')
     yield
     log.info("~~~~~~~~~~在所有请求之后执行一次~~~~~~~~~~~")
 
+
 @pytest.fixture(scope="function")
-#获取用例函数名称
+# 获取用例函数名称
 def testcase_data(request):
     testcase_name = request.function.__name__
     return api_data.get(testcase_name)
-
 
 
 def pytest_terminal_summary(terminalreporter, exitstatus, config):
@@ -47,7 +51,8 @@ def pytest_terminal_summary(terminalreporter, exitstatus, config):
     deselected = len(terminalreporter.stats.get('deselected', []))
     # terminalreporter._sessionstarttime 会话开始时间
     duration = time.time() - terminalreporter._sessionstarttime
-    email_list = {'用例总数':total,'通过':passed,'失败':failed,'错误':error,'跳过':skipped,'省略':deselected,'总运行时间':duration}
+    email_list = {'用例总数': total, '通过': passed, '失败': failed, '错误': error, '跳过': skipped, '省略': deselected,
+                  '总运行时间': duration}
     Send_email(result=email_list).mail()
     sum_list = ("用例总数: {}\n "
                 "通过: {}\n "
@@ -57,12 +62,13 @@ def pytest_terminal_summary(terminalreporter, exitstatus, config):
                 "省略: {}\n"
                 "总运行时间: {:.3f}s".format(total, passed, failed, error, skipped, deselected, duration))
     send_workwhat(sum_list)
+    send_workwhat_robot(sum_list)
     if failed > 0:
         sendServerChan(sum_list)
 
     print("===============pytest_terminal_summary===================")
     # print(terminalreporter.stats)
-    print("用例总数:",terminalreporter._numcollected)
+    print("用例总数:", terminalreporter._numcollected)
     print('通过:', len(terminalreporter.stats.get('passed', [])))
     print('失败:', len(terminalreporter.stats.get('failed', [])))
     print('错误:', len(terminalreporter.stats.get('error', [])))
@@ -71,6 +77,6 @@ def pytest_terminal_summary(terminalreporter, exitstatus, config):
     # terminalreporter._sessionstarttime 会话开始时间
     # duration = time.time() - terminalreporter._sessionstarttime
     print('总运行时间:', duration, 'seconds')
-    print('开始时间',terminalreporter._sessionstarttime)
-    print('现在时间',time.time())
-    print(time.time()-terminalreporter._sessionstarttime)
+    print('开始时间', terminalreporter._sessionstarttime)
+    print('现在时间', time.time())
+    print(time.time() - terminalreporter._sessionstarttime)
